31164c286bd8796bdd4edec60970e4a0afa1d73b
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-10 23:45:09 +0900

    :books: [Docs]: constants.ts 수정
    [styles]
    - regexrObj 객체 추가
    - css 상수 추가

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -32,8 +32,18 @@ export const colors = {
   green: '#22C58B',
 };
 
+export const regexrObj = {
+  signup: {
+    username: /^(?=.*[a-zA-Z])[^\sㄱ-ㅎㅏ-ㅣ!@#$%^&*()_\-+[\]{}=:;"'?/.,`~\\|<>]*$/, // 영문 또는 영문 + 숫자 (조합)
+    password: /^(?=.*[0-9])(?=.*[a-zA-Z])[^\sㄱ-ㅎㅏ-ㅣ{}[\]()/\\'"`~,;:.<>]*$/, // 영문 + 숫자 (포함)
+    tel: /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/,
+  },
+};
+
 export const inputMessage = {
   required: "필수 입력 항목입니다.",
   maxLen: (maxNum: number) => `최대 ${maxNum}자 까지 입력할 수 있습니다.`,
-  minLen: (minNum: number) => `최소 ${minNum}자 까지 입력해야 합니다.`
+  minLen: (minNum: number) => `최소 ${minNum}자 까지 입력해야 합니다.`,
 }
+
+export const headerHeight = '72px'; // 20 + 20 + 32
