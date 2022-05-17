f544a33ff1c023cff79a27e9f9f4a84c388d8c05
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-17 16:07:11 +0900

    :book: [Feat]: Implement MainDescription.tsx
    Implement MainDescription.tsx
    - 메인 페이지의 설명 컴포넌트 추가

---
+++ src/pages/Main/DescriptionBox.tsx
@@ -0,0 +1,158 @@
+import styled from 'styled-components';
+
+import { buttonStyle, colors, includes, media } from '../../styles';
+
+interface IPos {
+  tabletPos: string;
+  pcPos: string;
+}
+
+interface ILineSec {
+  tabletDeg: string;
+  pcDeg: string;
+}
+
+interface ILineThi {
+  tabletX: string;
+  tabletY: string;
+  pcX: string;
+  pcY: string;
+}
+
+interface IDescriptionBox {
+  posit: IPos;
+  lineSec: ILineSec;
+  lineThi: ILineThi;
+  prev: () => void;
+  next: () => void;
+  text: string;
+}
+
+function DescriptionBox({ posit, lineSec, lineThi, prev, next, text }: IDescriptionBox) {
+  console.log('DescBox');
+
+  return (
+    <Wrapper { ...posit }>
+      <Point />
+      <Line />
+      <LineSec { ...lineSec } />
+      <LineThi { ...lineThi } >
+        <Desc>
+          <Text>
+            {text}
+          </Text>
+          <Group>
+            <Prev type='button' onClick={prev}>{'이전'}</Prev>
+            <Next type='button' onClick={next}>{'다음'}</Next>
+          </Group>
+        </Desc>
+      </LineThi>
+    </Wrapper>
+  )
+}
+
+export default DescriptionBox;
+
+const Wrapper = styled.div<IPos>`
+  position: absolute;
+  top: ${(props) => props.tabletPos};
+  ${includes.flexBox('center', 'flex-start')}
+
+  @media ${media.pc_s} {
+    top: ${(props) => props.pcPos};
+  }
+`;
+
+const Point = styled.div`
+  flex-shrink: 0;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${colors.white};
+  box-shadow: 0 0 10px 2px ${(props) => props.theme.textColor};
+`;
+
+const Line = styled.div`
+  flex-shrink: 0;
+  width: 80px;
+  height: 1px;
+  border: 1px solid ${colors.white};
+  border-radius: 10px;
+
+  @media ${media.pc_s} {
+    width: 100px;
+  }
+`;
+
+const LineSec = styled.div<ILineSec>`
+  flex-shrink: 0;
+  width: 74px;
+  height: 1px;
+  border: 1px solid ${colors.white};
+  border-radius: 10px;
+  transform-origin: center left;
+  transform: rotate(${(props) => props.tabletDeg});
+
+  @media ${media.pc_s} {
+    width: 90px;
+    transform: rotate(${(props) => props.pcDeg});
+  }
+`;
+
+const LineThi = styled.div<ILineThi>`
+  position: relative;
+  flex-shrink: 0;
+  width: 50px;
+  height: 1px;
+  border: 1px solid ${colors.white};
+  border-radius: 10px;
+  transform:
+    translateX(${(props) => props.tabletX})
+    translateY(${(props) => props.tabletY});
+  
+  @media ${media.pc_s} {
+    transform:
+    translateX(${(props) => props.pcX})
+    translateY(${(props) => props.pcY});
+  }
+`;
+
+const Desc = styled.div`
+  position: absolute;
+  ${includes.flexBox('flex-start', 'center')}
+  flex-direction: column;
+  width: 270px;
+  border-radius: 10px;
+  padding: 15px;
+  background-color: ${(props) => props.theme.sameColor};
+  overflow: hidden;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .4);
+  transform: 
+    translateX(30px)
+    translateY(-17px);
+`;
+
+const Text = styled.p`
+  margin-bottom: 20px;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${(props) => props.theme.textColor};
+  letter-spacing: .5px;
+  line-height: 20px;
+`;
+
+const Group = styled.div`
+  ${includes.flexBox()}
+  align-self: flex-end;
+`;
+
+const Prev = styled.button`
+  ${buttonStyle.outline()}
+  width: 60px;
+  margin-right: 10px;
+`;
+
+const Next = styled.button`
+  ${buttonStyle.primary()}
+  width: 60px;
+`;
---
+++ src/pages/Main/MainDescription.tsx
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+import {
+  lineSecLocation,
+  lineThiLocation,
+  mainContextPaddingPc,
+  mainContextPaddingTablet,
+  media,
+  linePosition,
+  descText,
+  sidebarWidth,
+} from '../../styles';
+import { descState } from '../../global/atoms';
+import DescriptionBox from './DescriptionBox';
+
+function MainDescription() {
+  console.log('Description');
+
+  const [descStep, setDescStep] = useState(0);
+  const setDescState = useSetRecoilState(descState);
+  const onPrev = () => {
+    if (descStep <= 0) {
+      return;
+    }
+    setDescStep((prev) => prev - 1);
+  }
+  const onNext = () => {
+    setDescStep((prev) => prev + 1);
+    if (descStep >= 5) {
+      setDescState(false);
+      localStorage.removeItem('mainDesc');
+    }
+  }
+
+  return (
+    <DescList>
+      {descStep === 0 ?
+        <DescriptionBox
+          posit={linePosition.posit_1}
+          lineSec={lineSecLocation.Top}
+          lineThi={lineThiLocation.Top}
+          prev={onPrev}
+          next={onNext}
+          text={descText.text1} />
+        :
+        descStep === 1 ? 
+          <DescriptionBox
+            posit={linePosition.posit_2}
+            lineSec={lineSecLocation.Middle}
+            lineThi={lineThiLocation.Middle}
+            prev={onPrev}
+            next={onNext}
+            text={descText.text2} />
+          :
+          descStep === 2 ? 
+            <DescriptionBox
+              posit={linePosition.posit_3}
+              lineSec={lineSecLocation.Top}
+              lineThi={lineThiLocation.Top}
+              prev={onPrev}
+              next={onNext}
+              text={descText.text3} />
+            :
+            descStep === 3 ? 
+              <DescriptionBox
+                posit={linePosition.posit_4}
+                lineSec={lineSecLocation.Middle}
+                lineThi={lineThiLocation.Middle}
+                prev={onPrev}
+                next={onNext}
+                text={descText.text4} />
+              :
+              descStep === 4 ?
+                <DescriptionBox
+                  posit={linePosition.posit_5}
+                  lineSec={lineSecLocation.Bottom}
+                  lineThi={lineThiLocation.Bottom}
+                  prev={onPrev}
+                  next={onNext}
+                  text={descText.text5} />
+                :
+                <DescriptionBox
+                  posit={linePosition.posit_6}
+                  lineSec={lineSecLocation.Bottom}
+                  lineThi={lineThiLocation.Bottom}
+                  prev={onPrev}
+                  next={onNext}
+                  text={descText.text6} />
+      }
+    </DescList>
+  );
+}
+
+export default MainDescription;
+
+const DescList = styled.div`
+  position: absolute;
+  left: ${sidebarWidth.tablet};
+  z-index: 20;
+
+  @media ${media.pc_s} {
+    left: ${sidebarWidth.pc};
+  }
+`;
