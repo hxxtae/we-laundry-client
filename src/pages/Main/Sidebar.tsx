de65ce04b6badd0ab53b36f3bd30b3cc8bd4b184
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-17 17:56:08 +0900

    :book: [Feat]: Implement Sidebar.tsx
    Implement Sidebar Component
    - 메인 화면 side 메뉴바 추가

---
+++ src/pages/Main/Sidebar.tsx
@@ -0,0 +1,93 @@
+import { faPen, faReceipt, faAddressBook, faShirt, faBuilding, faChartColumn, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRecoilValue } from 'recoil';
+import '@fortawesome/fontawesome-svg-core';
+import styled from 'styled-components';
+
+import { colors, includes } from '../../styles';
+import { sidebarClickState, sidebarState } from '../../global/atoms';
+import SidebarItem from './SidebarItem';
+import { SidePaths } from '../../util';
+
+function Sidebar() {
+  console.log("Sidebar");
+
+  const sideToggle = useRecoilValue(sidebarState);
+  const sideClick = useRecoilValue(sidebarClickState);
+
+  return (
+    <AnimatePresence>
+      {sideToggle ? (
+        <Nav variants={navVariant} initial="init" animate="start" exit="end">
+          <List variants={listVariant}>
+            <SidebarItem path={SidePaths[0]} name='주문작성' click={sideClick} >
+              <FontAwesomeIcon icon={faPen} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={SidePaths[1]} name='주문내역' click={sideClick} >
+              <FontAwesomeIcon icon={faReceipt} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={SidePaths[2]} name='고객관리' click={sideClick} >
+              <FontAwesomeIcon icon={faAddressBook} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={SidePaths[3]} name='품목관리' click={sideClick} >
+              <FontAwesomeIcon icon={faShirt} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={SidePaths[4]} name='주소관리' click={sideClick} >
+              <FontAwesomeIcon icon={faBuilding} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={SidePaths[5]} name='결제내역' click={sideClick} >
+              <FontAwesomeIcon icon={faChartColumn} size="2x" />
+            </SidebarItem>
+            <SidebarItem path={''} name='영업관리' click={sideClick} >
+              <FontAwesomeIcon icon={faPowerOff} size="2x" />
+            </SidebarItem>
+          </List>
+        </Nav>) : null}
+    </AnimatePresence>
+  )
+}
+
+export default Sidebar;
+
+const navVariant = {
+  init: {
+    width: 0,
+  },
+  start: {
+    width: "auto",
+    transition: {
+      delayChildren: 0.5,
+    }
+  },
+  end: {
+    width: 0,
+    transition: {
+      delay: 0.5,
+    }
+  }
+}
+
+const listVariant = {
+  init: {
+    opacity: 0,
+  },
+  start: {
+    opacity: 1,
+  },
+  end: {
+    opacity: 0,
+  }
+}
+
+const Nav = styled(motion.nav)`
+  height: 100%;
+  background-color: ${colors.darkSlateSub};
+  transition: background-color 200ms ease-in-out;
+`;
+
+const List = styled(motion.ul)`
+  ${includes.flexBox('center', 'flex-start')}
+  flex-direction: column;
+`;
+
