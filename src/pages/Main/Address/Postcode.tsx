6bf187104b600fcbdcbcab239140fb705bf75510
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-23 22:12:26 +0900

    :book: [feat]: Daum Address Find API
    Postcode component
    - package.json : react-daum-postcode

---
+++ src/pages/Main/Address/Postcode.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import DaumPostcode from 'react-daum-postcode';
+
+import { buttonStyle } from '../../../styles';
+
+interface IPostcode {
+  setClose: (props: boolean) => void;
+  setData: (data: string) => void;
+}
+
+function Postcode({ setClose, setData }: IPostcode) {
+  const handleComplete = (data: any) => {
+    let fullAddress = data.address;
+    let extraAddress = ''; 
+    
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+      }
+      fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+    }
+
+    setClose(false);
+    setData(fullAddress);
+    console.log(fullAddress);
+  }
+
+  const onClose = () => {
+    setClose(false);
+  }
+
+  return (
+    <Box>
+      <DaumPostcode onComplete={handleComplete} />
+      <Button type='button' onClick={onClose}>닫기</Button>
+    </Box>
+  );
+}
+
+export default Postcode;
+
+const Box = styled.div`
+  width: 400px;
+`;
+
+const Button = styled.button`
+  ${buttonStyle.outline}
+  width: 100%;
+  margin-top: 5px;
+`;
