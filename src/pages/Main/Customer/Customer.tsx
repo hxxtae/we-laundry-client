a686934b4a4fa1e528282036660ed476c9bf7364
hxxtae <fkdlxmfkdl@gmail.com>
2022-05-30 23:55:14 +0900

    :book: [feat]: Implement page of Customer.tsx
    고객관리페이지 컴포넌트
    - customer.tsx (new)

--- src/pages/Main/Customer/Customer.tsx
+++ src/pages/Main/Customer/Customer.tsx
@@ -1,12 +1,18 @@
-import { useEffect } from 'react';
-import { useRouteMatch } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
-import { sidebarClickState } from '../../../global/atoms';
+import { useRouteMatch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import { sidebarClickState } from '../../../global';
+import { includes, media } from '../../../styles';
+import CustomerForm from './CustomerForm';
+import CustomerList from './CustomerList';
 
 function Customer() {
   console.log('Customer');
 
   const setSideClick = useSetRecoilState(sidebarClickState);
+  const [updateActive, setUpdateActive] = useState(false);
   const { path } = useRouteMatch();
   
   useEffect(() => {
@@ -14,10 +20,26 @@ function Customer() {
   }, []);
 
   return (
-    <div>
-      Customer!!
-    </div>
+    <Wrapper>
+      <CustomerForm
+        updateActive={updateActive}
+        setUpdateActive={setUpdateActive}/>
+      <CustomerList
+        setUpdateActive={setUpdateActive}/>
+    </Wrapper>
   )
 }
 
 export default Customer;
+
+const Wrapper = styled.div`
+  position: relative;
+  ${includes.flexBox('flex-start', 'flex-start')}
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  @media ${media.pc_s} {
+    width: auto;
+  }
+`;
