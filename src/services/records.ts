53389f7ae7dc823a38029934dc30e98239d44d50
hxxtae <fkdlxmfkdl@gmail.com>
2022-06-14 23:54:33 +0900

    :book: [feat]: Implement RecordOrder component
    - page/../Records/RecordsOrder.tsx
    - page/../Records/RecordsOrderList.tsx
    주문목록 리스트 컴포넌트 (품목 선택 시 주문목록에 리스트로 추가)
    
    + service/records.ts
    Api of Records Service

---
+++ src/services/records.ts
@@ -0,0 +1,95 @@
+import { AxiosResponse } from 'axios';
+import HttpClient from './http';
+
+// 세탁 주문 object
+export interface IRecordsOflaundry {
+  productId: string;
+  productName: string;
+  count: number;
+  price: number;
+};
+
+// 수선 주문 object
+export interface IRecordsOfRepair {
+  repairId: string;
+  repairName: string;
+  count: number;
+  price: number;
+};
+
+export interface IRecordObjResponse {
+  _id: string;
+  id: string;
+  recordDate: Date;
+  recordCount: number;
+  recordPrice: number;
+  cusid: string;
+  addid: string;
+  dong: string;
+  ho: string;
+  addfullname: string;
+  records: {
+    laundry: IRecordsOflaundry[];
+    repair: IRecordsOfRepair[];
+  }
+};
+
+export interface IRecordRequest {
+  id?: string;
+  recordCount: number;
+  recordPrice: number;
+  cusid: string;
+  addid: string;
+  addname: string;
+  dong: string;
+  ho: string;
+  addfullname: string;
+  laundry: IRecordsOflaundry[];
+  repair: IRecordsOfRepair[];
+}
+
+export interface IRecordSearchRequestByDate {
+  recordDate: Date;
+}
+
+export interface IRecordSearchRequestByAdd {
+  addname: string;
+  dong: string;
+  ho: string;
+}
+
+export interface IRecordSearchRequestByDongAndHo {
+  addname: string;
+  dong: string;
+  ho?: string;
+}
+
+interface IRecordsService {
+  createRecord: ({ recordCount, recordPrice, cusid, addid, addname, dong, ho, addfullname, laundry, repair }: IRecordRequest) => Promise<AxiosResponse>;
+}
+
+export default class RecordsService implements IRecordsService {
+  constructor(private http: HttpClient) {
+    this.http = http;
+  }
+
+  async createRecord({ recordCount, recordPrice, cusid, addid, addname, dong, ho, addfullname, laundry, repair }: IRecordRequest) {
+    const data = await this.http.fetch('/records', {
+      method: 'POST',
+      body: JSON.stringify({
+        recordCount,
+        recordPrice,
+        cusid,
+        addid,
+        addname,
+        dong,
+        ho,
+        addfullname,
+        laundry,
+        repair,
+      }),
+    });
+    return data;
+  }
+
+}
