e6fe72817f95bef913b3bb5d990d6b7a52a93486
hxxtae <fkdlxmfkdl@gmail.com>
2022-06-16 15:45:37 +0900

    :book: [feat]: Implement Popup in Records
    - RecordsReceiptPopup.tsx (접수 확인 팝업)
    - RecordsSuccess.tsx (접수 완료 팝업)

---
+++ src/pages/Main/Records/RecordPopups/RecordReceiptComponent/ReceiptSuccess.tsx
@@ -0,0 +1,166 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+import { recordLaundryState, recordReceiptExeState, recordRepairState, recordRequestState } from '../../../../../global';
+import { buttonStyle, colors, dragging, includes, media } from '../../../../../styles';
+
+interface IReceiptSuccess {
+  sumLaundry: { price: number, count: number };
+  sumRepair: { price: number, count: number };
+  setReceiptAct: React.Dispatch<React.SetStateAction<boolean>>;
+  setReceiptOkAct: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function ReceiptSuccess({ sumLaundry, sumRepair, setReceiptAct, setReceiptOkAct }: IReceiptSuccess) {
+  const resetLaundry = useResetRecoilState(recordLaundryState);
+  const resetRepair = useResetRecoilState(recordRepairState);
+  const resetRecord = useResetRecoilState(recordRequestState);
+  const setReceiptExeChk = useSetRecoilState(recordReceiptExeState);
+  const sumTotal = (sumLaundry.price + sumRepair.price).toLocaleString();
+
+  const onExit = () => {
+    setReceiptAct(false);
+    setReceiptOkAct(false);
+    setReceiptExeChk(true);
+
+    // request state 초기화
+    resetLaundry();
+    resetRepair();
+    resetRecord();
+  }
+
+  return (
+    <Wrapper variants={SuccessVariant} initial="init" animate="start">
+      <TitleBox>
+        <FontAwesomeIcon icon={faCheck}/>
+        <Title>{'접수 완료'}</Title>
+        <Desc>{'주문 접수가 완료되었습니다.'}</Desc>
+      </TitleBox>
+      <DetailList>
+        <DetailItem>
+          <Text>{'세탁 금액'}</Text>
+          <Sum>{`${sumLaundry.price.toLocaleString()}원`}</Sum>
+        </DetailItem>
+        <DetailItem>
+          <Text>{'수선 금액'}</Text>
+          <Sum>{`${sumRepair.price.toLocaleString()}원`}</Sum>
+        </DetailItem>
+        <DetailItem>
+          <Text>{'접수 금액'}</Text>
+          <Sum>{`${sumTotal}원`}</Sum>
+        </DetailItem>
+      </DetailList>
+      <Exit onClick={onExit}>{'닫기'}</Exit>
+    </Wrapper>
+  )
+}
+
+export default ReceiptSuccess;
+
+const SuccessVariant = {
+  init: {
+    opacity: 0,
+  },
+  start: {
+    opacity: 1,
+  }
+}
+
+const Wrapper = styled(motion.section)`
+  ${dragging.stop}
+  ${includes.flexBox()}
+  flex-direction: column;
+  width: 350px;
+  height: 430px;
+  padding: 24px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  border-radius: 4px;
+  background-color: ${(props) => props.theme.bgColor};
+  box-shadow: 0 0 30px rgba(0, 0, 0, .3);
+
+  @media ${media.pc_s} {
+    width: 400px;
+    height: 480px;
+  }
+`;
+
+const TitleBox = styled.div`
+  ${includes.flexBox()}
+  flex-direction: column;
+  color: ${(props) => props.theme.textColor};
+
+  svg:first-child {
+    font-size: 22px;
+    color: ${colors.green};
+  }
+
+  @media ${media.pc_s} {
+    svg:first-child {
+    font-size: 24px;
+  }
+  }
+`;
+
+const Title = styled.h2`
+  padding: 10px 0;
+  font-size: 18px;
+  font-weight: 600;
+  color: ${colors.green};
+
+  @media ${media.pc_s} {
+    padding: 15px 0;
+    font-size: 22px;
+  }
+`;
+
+const Desc = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+
+  @media ${media.pc_s} {
+    font-size: 18px;
+  }
+`;
+
+const DetailList = styled.ul`
+  flex-grow: 1;
+  width: 100%;
+  padding: 20px 10px;
+`;
+
+const DetailItem = styled.li`
+  ${includes.flexBox('center', 'space-between')}
+  padding: 5px 0;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  opacity: .7;
+
+  @media ${media.pc_s} {
+    font-size: 15px;
+  }
+`;
+
+const Sum = styled.strong`
+  font-size: 16px;
+  font-weight: 600;
+  @media ${media.pc_s} {
+    font-size: 18px;
+  }
+`;
+
+const Exit = styled.button`
+  ${buttonStyle.primary}
+  width: 100%;
+  height: 40px;
+
+  @media ${media.pc_s} {
+    height: 50px;
+  }
+`;
---
+++ src/pages/Main/Records/RecordPopups/RecordsReceiptPopup.tsx
@@ -0,0 +1,348 @@
+import { faArrowLeft, faCashRegister, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+import { recordLaundryState, recordRepairState, recordRequestState, recordsApi } from '../../../../global';
+import { buttonStyle, colors, dragging, includes, media, scroll, toastStyle } from '../../../../styles';
+import { LoadingComponent, Overlay } from '../../../../components';
+import { IRecordRequest } from '../../../../services/records';
+import ReceiptSuccess from './RecordReceiptComponent/ReceiptSuccess';
+
+interface IRecordReceiptPopup {
+  totalPay: { price: number, count: number };
+  setReceiptAct: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function RecordsReceiptPopup({ totalPay, setReceiptAct }: IRecordReceiptPopup) {
+  const laundry = useRecoilValue(recordLaundryState);
+  const repair = useRecoilValue(recordRepairState);
+  const recordState = useRecoilValue(recordRequestState);
+  const recordsService = useRecoilValue(recordsApi);
+  const [sumLaundry, setSumLaundry] = useState({price: 0, count: 0});
+  const [sumRepair, setSumRepair] = useState({ price: 0, count: 0 });
+  const [receiptOkAct, setReceiptOkAct] = useState(false);
+  
+  const { isLoading, mutate } = useMutation((_recordState: IRecordRequest) => recordsService.createRecord(_recordState));
+
+  useEffect(() => {
+    const sumLaundryObj = laundry.reduce((prev, curr) => ({
+      price: (prev.price + curr.price),
+      count: (prev.count + curr.count),
+    }), { price: 0, count: 0 });
+    const sumRepairObj = repair.reduce((prev, curr) => ({
+      price: (prev.price + curr.price),
+      count: (prev.count + curr.count),
+    }), { price: 0, count: 0 });
+
+    setSumLaundry(sumLaundryObj);
+    setSumRepair(sumRepairObj);
+  }, []);
+
+  const onReceipt = () => {
+    mutate(recordState, {
+      onSuccess: () => {
+        setReceiptOkAct(true);
+      },
+      onError: (error: any) => {
+        console.log('--Record Create Error--');
+        toastStyle.error(error.message);
+      }
+    });
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <LeftGroup>
+          <ListTitle>{'주문 내역'}</ListTitle>
+          <ReceiptList>
+            {laundry.map((obj) => (
+              <ReceiptItem
+                key={obj.productId}>
+                <Item>{obj.productName}</Item>
+                <Item>{obj.count}</Item>
+                <Item>{`${obj.price.toLocaleString()}원`}</Item>
+              </ReceiptItem>
+            ))}
+            {repair.map((obj) => (
+              <ReceiptItem
+                key={obj.repairId}>
+                <Item>{obj.repairName}</Item>
+                <Item>{obj.count}</Item>
+                <Item>{`${obj.price.toLocaleString()}원`}</Item>
+              </ReceiptItem>
+            ))}
+          </ReceiptList>
+          <ReceiptSumBox>
+            <ReceiptSum>
+              <strong>{'총 세탁 금액'}</strong>
+              <span>{`${sumLaundry.price.toLocaleString()}원`}</span>
+            </ReceiptSum>
+            <ReceiptSum>
+              <strong>{'총 수선 금액'}</strong>
+              <span>{`${sumRepair.price.toLocaleString()}원`}</span>
+            </ReceiptSum>
+            <ReceiptSum>
+              <strong>{'총액'}</strong>
+              <span>{`${totalPay.price.toLocaleString()}원`}</span>
+            </ReceiptSum>
+          </ReceiptSumBox>
+        </LeftGroup>
+
+        <RightGroup>
+          <ReceiptAmount>
+            <AmountTitle>{'결제 금액'}</AmountTitle>
+            <Amount>{`${totalPay.price.toLocaleString()}원`}</Amount>
+          </ReceiptAmount>
+          <ReceiptSale>
+            <SaleButton type='button'>
+              <p>
+                {'할인'}
+                <FontAwesomeIcon icon={faChevronRight} />
+              </p>
+            </SaleButton>
+          </ReceiptSale>
+          <ReceiptPayBox>
+            <ReceiptPayment onClick={onReceipt}>
+              <FontAwesomeIcon icon={faCashRegister} />
+              {'접수하기'}
+            </ReceiptPayment>
+            <ReceiptCancel
+              type='button'
+              onClick={() => setReceiptAct(false)} >
+              <FontAwesomeIcon icon={faArrowLeft} />
+              {'뒤로가기'}
+            </ReceiptCancel>
+          </ReceiptPayBox>
+        </RightGroup>
+      </Wrapper>
+
+      
+      {isLoading && 
+        <Overlay>
+          <LoadingComponent loadingMessage='잠시만 기다려주세요.' />
+        </Overlay>}
+      {receiptOkAct && 
+        <Overlay>
+          <ReceiptSuccess
+            sumLaundry={sumLaundry}
+            sumRepair={sumRepair}
+            setReceiptAct={setReceiptAct}
+            setReceiptOkAct={setReceiptOkAct} />
+        </Overlay>}
+    </>
+  )
+}
+
+export default RecordsReceiptPopup;
+
+const Wrapper = styled.section`
+  ${dragging.stop}
+  position: absolute;
+  ${includes.flexBox("flex-start", "flex-start")}
+  width: 100%;
+  height: 100%;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  border-radius: 4px;
+  background-color: ${(props) => props.theme.bgColor};
+  transition: background-color border-color 200ms ease-in-out;
+  overflow: hidden;
+  z-index: 11;
+`;
+
+const LeftGroup = styled.div`
+  ${includes.flexBox('flex-start', 'space-between')}
+  flex-direction: column;
+  width: 300px;
+  height: 100%;
+  padding: 20px;
+  background-color: ${(props) => props.theme.bgColor};
+
+  @media ${media.pc_s} {
+    width: 350px;
+  }
+`;
+
+const RightGroup = styled.div`
+  ${includes.flexBox('flex-start', 'space-between')}
+  flex-direction: column;
+  flex-grow: 1;
+  height: 100%;
+  padding: 30px;
+  background-color: ${(props) => props.theme.bgColorBlur};
+`;
+
+const ListTitle = styled.h2`
+  font-size: 14px;
+  font-weight: 600;
+  color: ${colors.blueDark};
+  margin-bottom: 10px;
+
+  @media ${media.pc_s} {
+    font-size: 16px;
+  }
+`;
+
+const ReceiptList = styled.ul`
+  ${includes.flexBox('center', 'flex-start')}
+  flex-direction: column;
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  flex-grow: 1;
+  ${(props) => scroll.custom(8, props.theme.borderColorSub, props.theme.textColor)}
+`;
+
+const ReceiptItem = styled.li`
+  ${includes.flexBox('center', 'space-between')}
+  width: 100%;
+  padding: 10px 0;
+  color: ${(props) => props.theme.textColor};
+  font-size: 14px;
+
+  @media ${media.pc_s} {
+    font-size: 16px;
+  }
+`;
+
+const Item = styled.span`
+  &:nth-of-type(2) {
+    margin-left: 10px;
+    color: ${colors.blue};
+  }
+
+  &:last-child {
+    font-size: 15px;
+    margin-left: auto;
+  }
+
+  @media ${media.pc_s} {
+    &:last-child {
+      font-size: 16px;
+    }
+  }
+`;
+
+const ReceiptSumBox = styled.ul`
+  ${includes.flexBox()}
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ReceiptSum = styled.li`
+  ${includes.flexBox('center', 'space-between')}
+  width: 100%;
+  padding: 18px 0;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => props.theme.textColor};
+
+  &:last-child {
+    padding-bottom: 0px;
+  }
+
+  @media ${media.pc_s} {
+    font-size: 18px;
+  }
+`;
+
+const ReceiptAmount = styled.div`
+  flex-grow: 1;
+  ${includes.flexBox('flex-start', 'flex-start')}
+  flex-direction: column;
+  width: 100%;
+  font-weight: 600;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const AmountTitle = styled.h2`
+  font-size: 16px;
+
+  @media ${media.pc_s} {
+    font-size: 18px;
+  }
+`;
+
+const Amount = styled.strong`
+  padding: 12px 0;
+  font-size: 34px;
+  color: ${colors.blue};
+
+  @media ${media.pc_s} {
+    font-size: 44px;
+  }
+`;
+
+const ReceiptSale = styled.div`
+  width: 100%;
+  padding: 15px 0;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const SaleButton = styled.button`
+  ${includes.flexBox('flex-start', 'flex-start')}
+  ${buttonStyle.outline}
+  width: 200px;
+  height: 70px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+  
+  svg {
+    padding-left: 5px;
+  }
+
+  @media ${media.pc_s} {
+    width: 220px;
+    height: 80px;
+    font-size: 15px;
+  }
+`;
+
+const ReceiptPayBox = styled.div`
+  ${includes.flexBox()}
+  padding-top: 15px;
+  ${dragging.stop}
+`;
+
+const ReceiptPayment = styled.button`
+  ${includes.flexBox()}
+  flex-direction: column;
+  ${buttonStyle.primary}
+  width: 130px;
+  height: 80px;
+  margin-right: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+
+  svg {
+    font-size: 22px;
+    margin-bottom: 5px;
+  }
+
+  @media ${media.pc_s} {
+    width: 150px;
+    height: 90px;
+  }
+`;
+
+const ReceiptCancel = styled.button`
+  ${includes.flexBox()}
+  flex-direction: column;
+  ${buttonStyle.secondary}
+  width: 130px;
+  height: 80px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+
+  svg {
+    font-size: 22px;
+    margin-bottom: 5px;
+  }
+
+  @media ${media.pc_s} {
+    width: 150px;
+    height: 90px;
+  }
+`;
+
