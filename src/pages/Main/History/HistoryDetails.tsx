945b233e51b6ad2300a0f00f978fbc4860ec50d1
hxxtae <fkdlxmfkdl@gmail.com>
2022-06-21 23:46:17 +0900

    :book: [feat]: Implement HistoryDetailList
    - page/../HistoryDetailList.tsx (New)
    주문내역 상세 리스트 (컴포넌트 분리)
    - page/../HistoryDetail.tsx (Update)

--- src/pages/Main/History/HistoryDetails.tsx
+++ src/pages/Main/History/HistoryDetails.tsx
@@ -1,19 +1,187 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { includes } from '../../../styles';
+
+import { buttonStyle, colors, dragging, includes, media, toastStyle } from '../../../styles';
+import { deleteState, recordRequestState, recordsApi } from '../../../global';
+import { DeleteConfirm, LoadingComponent, Overlay } from '../../../components';
+import { dateToString } from '../../../components/DateComponent';
+import { queryKeys } from '../../../util';
+import HistoryDetailList from './HistoryDetailList';
 
 function HistoryDetails() {
+  const recordState = useRecoilValue(recordRequestState);
+  const recordService = useRecoilValue(recordsApi);
+  const [deletePopup, setDeletePopup] = useRecoilState(deleteState);
+  const client = useQueryClient();
+
+  const { isLoading, mutate } = useMutation((id: string) => recordService.deleteRecord(id));
+
+  const onUpdateClick = () => {
+    alert('준비중 입니다. *^^*');
+  }
+
+  const onDeleteClick = () => {
+    if (!recordState.id) {
+      alert('주문 내역을 선택해 주세요.');
+      return;
+    }
+    setDeletePopup(true);
+  }
+
+  const onDelete = (id: string) => {
+    const recordId = id;
+    if (!recordId) return;
+
+    mutate(recordId, {
+      onSuccess: () => {
+        toastStyle.info('주문 내역이 삭제되었습니다.');
+        const { recordDate, addname, dong } = recordState;
+        if (!recordDate) {
+          client.invalidateQueries(queryKeys.records.list());
+          return;
+        }
+        const formatDate = dateToString(new Date(recordDate));
+        client.invalidateQueries(queryKeys.records.listDate(formatDate));
+        client.invalidateQueries(queryKeys.records.listDong(addname, dong));
+      },
+      onError: (error: any) => {
+        toastStyle.error(error.message);
+        console.log('--Record Delete Error--');
+      }
+    })
+  }
+
   return (
-    <Wrapper>
-      Detail
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Title>{'주문 상세'}</Title>
+        <GroupBox>
+          <SumGroup>
+            <SumTitle>{'총 주문 금액'}</SumTitle>
+            <Sum>{`${recordState.recordPrice.toLocaleString()}원`}</Sum>
+          </SumGroup>
+          <ReceiptGroup>
+            <ReceiptTitle>{'주문 내역'}</ReceiptTitle>
+            <HistoryDetailList />
+          </ReceiptGroup>
+        </GroupBox>
+        <Control>
+          <UpdateButton type='button' onClick={onUpdateClick}>{`내역 수정`}</UpdateButton>
+          <DeleteButton type='button' onClick={onDeleteClick}>{`내역 삭제`}</DeleteButton>
+        </Control>
+      </Wrapper>
+
+      {deletePopup && 
+        <Overlay>
+          <DeleteConfirm deleteId={recordState.id || ''} onDelete={onDelete} setDeletePop={setDeletePopup} />
+        </Overlay>}
+      {isLoading && 
+        <Overlay>
+          <LoadingComponent loadingMessage='잠시만 기다려주세요.' />
+        </Overlay>}
+    </>
   )
 }
 
 export default HistoryDetails;
 
 const Wrapper = styled.section`
+  flex-grow: 1;
+  ${dragging.stop}
   ${includes.flexBox('flex-start', 'flex-start')}
   flex-direction: column;
-  flex-grow: 1;
-  border-left: 1px solid ${(props) => props.theme.borderColor};
+  padding-top: 10px;
+  margin-left: 30px;
+  align-items: stretch;
+
+  @media ${media.pc_s} {
+    padding-top: 13px;
+    margin-left: 40px;
+  }
+
+  @media ${media.pc_l} {
+    width: 582px;
+  }
 `;
+
+const Title = styled.h2`
+  color: ${(props) => props.theme.textColor};
+  font-size: 14px;
+  font-weight: 600;
+
+  @media ${media.pc_s} {
+    font-size: 16px;
+  }
+`;
+
+const GroupBox = styled.div`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const SumGroup = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const SumTitle = styled.h3`
+  margin-bottom: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  color: ${colors.blueDark};
+
+  @media ${media.pc_s} {
+    font-size: 15px;
+  }
+`;
+
+const Sum = styled.span`
+  font-size: 30px;
+  font-weight: 600;
+
+  @media ${media.pc_s} {
+    font-size: 38px;
+  }
+`;
+
+const ReceiptGroup = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const ReceiptTitle = styled.h3`
+  font-size: 14px;
+  font-weight: 600;
+  color: ${colors.blueDark};
+
+  @media ${media.pc_s} {
+    font-size: 15px;
+  }
+`;
+
+const Control = styled.div`
+  ${includes.flexBox('center', 'flex-start')}
+  margin-top: auto;
+`;
+
+const UpdateButton = styled.button`
+  ${buttonStyle.outline}
+  width: 100px;
+  height: 40px;
+  margin-right: 5px;
+
+  @media ${media.pc_s} {
+    width: 140px;
+  }
+`;
+
+const DeleteButton = styled.button`
+  ${buttonStyle.close}
+  width: 100px;
+  height: 40px;
+
+  @media ${media.pc_s} {
+    width: 140px;
+  }
+`;
+
