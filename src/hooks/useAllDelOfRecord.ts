48c5e1003dcb3cbbcbbc98bacd4cbaff68cc6872
Heetae Kim <fkdlxmfkdl@gmail.com>
2022-06-23 16:55:56 +0900

    :recycle: [refactor]: Refact Control Button
    - Refactoring Control button function of Record

--- src/hooks/useAllDelOfRecord.ts
+++ src/hooks/useAllDelOfRecord.ts
@@ -1,3 +1,45 @@
-export const useAllDelOfRecord = () => {
+import { IRecordsOflaundry, IRecordsOfRepair } from "../services/records";
 
+type ISetState = IRecordsOflaundry | IRecordsOfRepair;
+
+export const useAllDelOfRecord = <T extends ISetState>(
+  setState: React.Dispatch<React.SetStateAction<T[]>>,
+  clickItems: string[],
+  setClickItems: React.Dispatch<React.SetStateAction<string[]>>
+  ) => {
+  
+  const onAllRemove = () => {
+    setState((prevObjs) => {
+      const copyLaundrys = prevObjs
+        .map((obj) => {
+          if (clickItems.includes((<IRecordsOflaundry>obj).productId || (<IRecordsOfRepair>obj).repairId)) {
+            setClickItems((prevItems) => {
+              const copyClickItems = [...prevItems];
+              const index = copyClickItems.indexOf((<IRecordsOflaundry>obj).productId || (<IRecordsOfRepair>obj).repairId);
+              if (index === -1) {
+                return prevItems;
+              }
+              copyClickItems.splice(index, 1);
+              return copyClickItems;
+            });
+
+            return {
+              ...obj,
+              productId: '',
+              repairId: '',
+            };
+          }
+          return obj;
+        })
+        .filter((obj) => (<IRecordsOflaundry>obj).productId || (<IRecordsOfRepair>obj).repairId);
+      
+      if (!clickItems.length) {
+        return [];
+      }
+      
+      return copyLaundrys;
+    });    
+  };
+
+  return onAllRemove;
 }
