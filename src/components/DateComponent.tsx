6e003a0cb062d5e1b3c399a6cd2c8f50e1575fcc
hxxtae <fkdlxmfkdl@gmail.com>
2022-11-03 14:21:54 +0900

    :recycle: [refactory]: Update DateComponent
    - Update DateComponent in common components

--- src/components/DateComponent.tsx
+++ src/components/DateComponent.tsx
@@ -6,21 +6,49 @@ import ReactDatePicker from 'react-datepicker';
 import styled from 'styled-components';
 
 import { colors, includes, inputStyle, media } from '../styles';
+import { useState } from 'react';
 
-export const dateToString = (date: Date) => {
-  return date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+interface IRenderDateHeader {
+  // Ref: renderCustomHeader property in interfaece of ReactDatePickerProps
+  date: Date;
+  changeYear(year: number): void;
+  changeMonth(month: number): void;
+  decreaseMonth(): void;
+  increaseMonth(): void;
+  prevMonthButtonDisabled: boolean;
+  nextMonthButtonDisabled: boolean;
+  decreaseYear(): void;
+  increaseYear(): void;
+  prevYearButtonDisabled: boolean;
+  nextYearButtonDisabled: boolean;
 }
-
-const months = [
-  "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"
-];
 
 interface IDateComponent {
   thisDate: string;
   setThisDate: React.Dispatch<React.SetStateAction<string>>;
 }
 
+export const dateToString = (date: Date) => {
+  return date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+}
+
 function DateComponent({ thisDate, setThisDate }: IDateComponent) {
+  const [months] = useState([
+    "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"
+  ]);
+
+  const renderCustomHeader = ({ date, decreaseMonth, increaseMonth, prevMonthButtonDisabled, nextMonthButtonDisabled }: IRenderDateHeader) => (
+    <CalendarHeader>
+      <button type='button' onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <p>{date.getFullYear()}년 {months[date.getMonth()]}</p>
+      <button type='button' onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+        <FontAwesomeIcon icon={faChevronRight} />
+      </button>
+    </CalendarHeader>
+  )
+
   return (
     <Wrapper>
       <ReactDatePicker
@@ -37,23 +65,7 @@ function DateComponent({ thisDate, setThisDate }: IDateComponent) {
         }}
         disabledKeyboardNavigation // 다른 월의 같은 날짜 자동 seleted 되는 현상 방지
         showPopperArrow={false}
-        renderCustomHeader={({ // header 커스텀 설정
-          date,
-          decreaseMonth,
-          increaseMonth,
-          prevMonthButtonDisabled,
-          nextMonthButtonDisabled,
-        }) => (
-          <CalendarHeader>
-            <button type='button' onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-            <p>{date.getFullYear()}년 {months[date.getMonth()]}</p>
-            <button type='button' onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          </CalendarHeader>
-        )}
+        renderCustomHeader={renderCustomHeader}
       />
     </Wrapper>
   )
